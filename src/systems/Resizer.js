7319d8b741fec159b685992e519a77f2808d0737
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-10-06 15:06:30 +0200

    adding resize on renderer to make page responsiv

--- src/systems/Resizer.js
+++ src/systems/Resizer.js
@@ -1,4 +1,4 @@
-const setSize (container, camera, renderer) => {
+const setSize = (container, camera, renderer) => {
  
     // Set the camera's aspect ratio
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -11,13 +11,21 @@ const setSize (container, camera, renderer) => {
 
     // set the pixel ratio (for mobile devices)
     renderer.setPixelRatio(window.devicePixelRatio);
-  
+  };
 
-};
  class Resizer{
   constructor(container, camera, renderer) {
     //set initial size on load
-    setSize(container, camera, renderer)
+    setSize(container, camera, renderer);
+
+    window.addEventListener('resize', () =>{
+      //set size again if resize occurs
+      setSize(container, camera, renderer);
+
+      this.onResize();
+    });
   }
+  //passing a hook to world.js and then adding it to renderer if resized
+  onResize(){}
  }
 export { Resizer };
